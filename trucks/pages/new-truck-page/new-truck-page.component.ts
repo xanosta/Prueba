675c1725850f6139ea9c3fadec31f4fc2ea7eb65
WXoa <pablo.rego13@gmail.com>
2025-06-30 04:43:37 +0200

    Delete trucks directory

--- trucks/pages/new-truck-page/new-truck-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { PageHeaderComponent } from '../../../../../../shared/components/page-header/page-header.component';
-
-@Component({
-  templateUrl: './new-truck-page.component.html',
-  imports: [PageHeaderComponent],
-})
-export class NewTruckPageComponent {}
