4a60101f71746149ec8ff07806199c642906449a
WXoa <pablo.rego13@gmail.com>
2025-07-01 09:22:44 +0200

    Delete tractor-heads directory

--- tractor-heads/services/types/sse-tractor-head-filters.ts
+++
@@ -1,8 +0,0 @@
-import { TractorHeadFilters } from "../../models/tractor-head-filters";
-
-type RequiredFilters = {
-    locationId: number;
-    token: string;
-};
-
-export type SseTractorHeadFilters = RequiredFilters & Partial<TractorHeadFilters>;
