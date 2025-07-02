2f6b0b4501383cce7a051a4addff33db00c82085
WXoa <pablo.rego13@gmail.com>
2025-07-02 11:27:48 +0200

    Delete tractor-heads directory

--- tractor-heads/store/updaters/reset-tractor-head-filters.updater.ts
+++
@@ -1,8 +0,0 @@
-import { PartialStateUpdater } from '@ngrx/signals';
-import { TractorHeadsSlice, tractorHeadsSliceInitialValue } from '../tractor-heads.slice';
-
-export function resetTractorHeadFiltersUpdater(): PartialStateUpdater<TractorHeadsSlice> {
-  return () => ({
-    _filters: tractorHeadsSliceInitialValue._filters,
-  });
-}
