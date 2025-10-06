a5fb2444b318e897be6cdb8a7d9d210fbc844a3f
WXoa <pablo.rego13@gmail.com>
2025-10-06 11:17:18 +0200

    Delete devices directory

--- devices/store/updaters/set-devices.updater.ts
+++
@@ -1,9 +0,0 @@
-import { PartialStateUpdater } from '@ngrx/signals';
-import { DevicesSlice } from '../devices.slice';
-import { Location } from '../../models/device.model';
-
-export function setDevicesUpdater(locations: Location[]): PartialStateUpdater<DevicesSlice> {
-  return () => ({
-    _locations: locations,
-  });
-}
